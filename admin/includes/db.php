2b8c5d1676a0fae78fc1323986c68ae83d016ea1
ma <92aggerholm@gmail.com>
2021-06-01 20:23:21 +0200

    refactoring

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -8,6 +8,7 @@ $conn = new mysqli($db_host, $db_username ,$db_password ,$db_name);
 if($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
+
 //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 //$conn->set_charset("utf8mb4");
 ?>
