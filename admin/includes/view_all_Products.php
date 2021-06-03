c20d5a25388fcb14534344f6800664c48d435755
ma <92aggerholm@gmail.com>
2021-06-03 20:29:35 +0200

    finished view_all_Products.php

--- admin/includes/view_all_Products.php
+++ admin/includes/view_all_Products.php
@@ -43,15 +43,42 @@
                 <td>$product_description</td>
                 <td><img style="width:50px;" src='../images/$product_image' alt="Product image" /></td>
                 <td>$product_rating</td>
-                <td>$product_price</td>
-                <td>$product_category_id</td>
-                <td>$product_country_id</td>
+                <td>$product_price Kr.-</td>
+        USERS_ONE;
+
+        $stmt = $conn->prepare("SELECT * FROM categories WHERE category_id = ?");
+        $stmt->bind_param("i", $product_category_id);
+        $stmt->execute();
+        $get_categories_by_id = $stmt->get_result(); // get the mysqli result
+        $stmt->close();
+
+        while($row = mysqli_fetch_assoc($get_categories_by_id)) {
+            $category_id = $row['category_id'];
+            $category_title = $row['category_title'];
+
+            echo "<td>{$category_title}</td>";
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE country_id = ?");
+        $stmt->bind_param("i", $product_country_id);
+        $stmt->execute();
+        $get_countries_by_id = $stmt->get_result(); // get the mysqli result
+        $stmt->close();
+
+        while($row = mysqli_fetch_assoc($get_countries_by_id)) {
+            $country_id = $row['country_id'];
+            $country_title = $row['country_title'];
+
+            echo "<td>{$country_title}</td>";
+        }
+
+        echo <<<USERS_TWO
                 <td>$product_editedDate</td>
                 <td>$product_createdDate</td>
                 <td><a class="btn btn-primary" href='products.php?source=edit_product&p_id={$product_id}'>Edit</a></td>
                 <td><a class="btn btn-danger" onclick="return confirm('Are you sure you want to delete {$product_title}?');" href='products.php?delete={$product_id}'>Delete</a></td>
             </tr>
-        USERS_ONE;
+        USERS_TWO;
     }
 
     // Delete user
