d0be8a2194d40cd8243aa8ce1b1d99602241a25d
MichaelAggerholm <61044186+MichaelAggerholm@users.noreply.github.com>
2021-06-29 10:23:14 +0200

    changed this back to live.
    changed this back to live.

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_name = "banan";
+$db_name = "banan_salg";
 
 $conn = new mysqli($db_host, $db_username ,$db_password ,$db_name);
 if($conn->connect_error) {
